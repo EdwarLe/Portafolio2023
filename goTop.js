c56124873fa51d2c716b03459386ea20d7da4eef
Edwar León <edwar.leon.m@gmail.com>
2023-05-12 19:08:10 -0500

    Modificación de menú

--- goTop.js
+++ goTop.js
@@ -1,8 +1,12 @@
 const btnScrollTop = document.getElementById ("btn_up")
+const menuOculto = document.getElementById('home')
+const menuPlegable = document.getElementById('menu_pleg')
 
 window.addEventListener('load', () => {
     document.getElementById("loader").classList.toggle("loader2")
 })
+
+/* Función para subir la página cuando se da click en el botón */
 
 btnScrollTop.addEventListener('click', () => {
     window.scrollTo(0, 0)
@@ -10,6 +14,8 @@ btnScrollTop.addEventListener('click', () => {
 
 window.onscroll = () => {
     addBtnScroll()
+    hideMenu()
+    showMenuPlegable()
 }
 
 const addBtnScroll = () => {
@@ -17,5 +23,25 @@ const addBtnScroll = () => {
         btnScrollTop.classList.remove("btn-up-on")
     } else {
         btnScrollTop.classList.add("btn-up-on")
+    }
+}
+
+/* Función para mostrar el menú plegable */
+
+const showMenuPlegable = () => {
+    if (window.scrollY < 150) {
+        menuPlegable.classList.add('show')
+    } else {
+        menuPlegable.classList.remove('show')
+    }
+}
+
+/* Función para ocultar el menú */
+
+const hideMenu = () => {
+    if (window.scrollY < 150) {
+        menuOculto.classList.add('hide')
+    } else {
+        menuOculto.classList.remove('hide')
     }
 }
