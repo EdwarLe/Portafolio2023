a3c79fbe41dfa52b78b21207f3c6d16e310499f9
Edwar León <edwar.leon.m@gmail.com>
2023-07-17 20:42:44 -0500

    Actualización agregando el e-commerce

--- functions/select-pages.js
+++ functions/select-pages.js
@@ -1,7 +1,14 @@
-const numSelect = document.querySelectorAll(".num__select > p")
+const numSelect = document.querySelectorAll(".num__select > p");
+const aboutP = document.querySelectorAll(".about__p");
 
+for (let i = 0; i < numSelect.length; i++) {
+    let count;
+  numSelect[i].addEventListener("click", function () {
+    let numValue = parseInt(numSelect[i].textContent);
+    if(numValue === i + 1) {
+        count = numValue
+    }
+    
+  });
 
-
-for(let i = 0; i < numSelect.length; i++) {
-    console.log(numSelect[i])
 }
